2b973844d763ada8b019df9d817bbfd818a06346
Hamza Mirza <hamza665@hotmail.de>
2025-01-13 11:26:55 +0100

    unit test for calcTotalCookingTime func added

--- tests/helperFunctions.test.js
+++ tests/helperFunctions.test.js
@@ -1,3 +1,25 @@
 import { calcTotalCookingTime } from "../utils/helperFunctions";
 
-test("should ", () => {});
+describe("calcTotalCookingTime function works properly", () => {
+  test("should return correct total time", () => {
+    const wishList = [
+      { cookTimeMinutes: 10 },
+      { cookTimeMinutes: 20 },
+      { cookTimeMinutes: 30 },
+    ];
+    const result = calcTotalCookingTime(wishList);
+    expect(result).toBe(60);
+  });
+
+  test("should return 0 if given array is empty", () => {
+    const wishList = [];
+    const result = calcTotalCookingTime(wishList);
+    expect(result).toBe(0);
+  });
+
+  test("should return zero when given array is not an array", () => {
+    const wishList = "not an array";
+    const result = calcTotalCookingTime(wishList);
+    expect(result).toBe(0);
+  });
+});
