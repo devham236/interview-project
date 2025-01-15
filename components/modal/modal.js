de03ce9f2cc2561b5c7982e09dcf4912b441dba2
Hamza Mirza <hamza665@hotmail.de>
2025-01-15 09:26:48 +0100

    restructured file organization

---
+++ components/modal/modal.js
@@ -0,0 +1,24 @@
+const modalEl = document.getElementById("modal");
+let currentModal = null;
+
+export function openModal(item) {
+  currentModal = item;
+
+  modalEl.innerHTML = `<div id="modal-content">
+            <h2 id="modal-title">${currentModal.name}</h2>
+            <ul>
+                ${currentModal.ingredients
+                  .map((ing) => `<li>${ing}</li>`)
+                  .join("")}
+            </ul>
+            <button class='modal-close-btn'>close</button>
+        </div>`;
+
+  const modalCloseBtn = document.querySelector(".modal-close-btn");
+  modalCloseBtn.addEventListener("click", closeModal);
+}
+
+export function closeModal() {
+  currentModal = null;
+  modalEl.innerHTML = "";
+}
